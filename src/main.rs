c8d95a9af6a183c7539415b1505d99e7f3ad1f3a
ArunJot <arunjot@antiersolutions.com>
2025-04-28 13:30:22 +0530

    chore: Added the dependency for diesel

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use jsonrpsee::core::client::ClientT;
-use jsonrpsee::ws_client::WsClientBuilder;
 use std::io;
+mod blockchain_data;
+pub use blockchain_data::*;
+use jsonrpsee::ws_client::{WsClient, WsClientBuilder};
+
 
 #[tokio::main]
 async fn main() {
@@ -17,7 +19,7 @@ async fn main() {
     if chars.next() == Some((0, 'w')) && chars.next() == Some((1, 's')) {
         println!("Checking the connection...");
 
-        let client_result = WsClientBuilder::default().build(endpoint.clone()).await;
+        let client_result = WsClientBuilder::default().build(&endpoint.clone()).await;
         if client_result.is_ok() {
             println!("Connection Established!");
         } else {
@@ -36,7 +38,7 @@ async fn main() {
         let selected_option: u32 = option_input.trim().parse().expect("Please type a number!");
 
         match selected_option {
-            1 => fetch_blockchain_name(&endpoint).await,
+            1 => fetch_blockchain_name(client_result.expect("msg")).await,
             _ => println!("Invalid option selected."),
         }
     } else {
@@ -44,23 +46,10 @@ async fn main() {
     }
 }
 
-async fn fetch_blockchain_name(endpoint: &str) {
-    match get_blockchain_name(endpoint).await {
+async fn fetch_blockchain_name(client: WsClient) {
+    match get_blockchain_name(client).await {
         Ok(name) => println!("Blockchain name: {:?}", name),
         Err(err) => println!("Error retrieving blockchain name: {}", err),
     }
 }
-
-async fn get_blockchain_name(endpoint: &str) -> Result<String, std::io::Error> {
-    let client = WsClientBuilder::default()
-        .build(endpoint)
-        .await
-        .expect("Failed to build WebSocket client");
-
-    let chain_name: String = client
-        .request("system_chain", jsonrpsee::core::params::ArrayParams::new())
-        .await
-        .expect("Failed to retrieve the chain name");
-
-    Ok(chain_name)
-}
+ 
