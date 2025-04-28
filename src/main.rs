2376c088033ef54a463a65870d375be1c28f7436
ArunJot <arunjot@antiersolutions.com>
2025-04-28 12:56:34 +0530

    chore: Added the functionality of fetching blockchain name

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use jsonrpsee::core::client::ClientT;
+use jsonrpsee::ws_client::WsClientBuilder;
 use std::io;
-fn main() {
+
+#[tokio::main]
+async fn main() {
     println!("Hello! Welcome to the Blockchain World");
     println!("Please enter the websocket data for the blockchain");
 
@@ -9,11 +13,23 @@ fn main() {
         .expect("Failed to read line");
 
     let mut check: std::str::CharIndices<'_> = new.char_indices();
-    if check.next() == Some((0, 'w')) && check.next() ==  Some((1,'s')) {
+    if check.next() == Some((0, 'w')) && check.next() == Some((1, 's')) {
         println!("Checking the data");
-    }
-    else{
+
+        match get_blockchain_name(&new).await {
+            Ok(name) => println!("Blockchain name is {:?}", name),
+            Err(err) => println!("Error is {}", err),
+        }
+    } else {
         println!("Please enter the correct endpoint");
     }
 
+    pub async fn get_blockchain_name(url: &str) -> Result<String, std::io::Error> {
+        let client = WsClientBuilder::default().build(url).await.expect("REASON");
+        let chain_name: String = client
+            .request("system_chain", jsonrpsee::core::params::ArrayParams::new())
+            .await
+            .expect("Failed to retrieve the chain name");
+        Ok(chain_name)
+    }
 }
