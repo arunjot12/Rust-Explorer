443d7dc06e712d2a3401b993df797ad40f8ed425
ArunJot <arunjot@antiersolutions.com>
2025-04-28 16:01:59 +0530

    chore: Added the migration files for the database

---
+++ migrations/2025-04-28-101729_blockchain_info/up.sql
@@ -0,0 +1,5 @@
+-- Your SQL goes here
+CREATE TABLE blockchain_info (
+   id SERIAL PRIMARY KEY,
+   blockchain_name VARCHAR NOT NULL
+);
---
+++ src/schema.rs
@@ -0,0 +1,8 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    blockchain_info (id) {
+        id -> Int4,
+        blockchain_name -> Varchar,
+    }
+}
